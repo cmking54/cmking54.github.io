b8180fc4c0a747d8c7d0c577f214ba9c45e3a19c
cmking54 <cmking5496@gmail.com>
2019-09-11 15:01:38 -0400

    get Cookie fix

--- js/cookie.js
+++ js/cookie.js
@@ -5,21 +5,38 @@ function makeCookie(key, value, hours_till_expire=1) {
   document.cookie = key + "=" + value + ";" + expires + ";path=/;";
 }
 
-function getCookie(name) {
-  var name_pattern = new RegExp(name + "=(.*)");
-  var decodedCookie = decodeURIComponent(document.cookie); // why needed?
-  //console.log("Decoded: " + decodedCookie);
-  var crumbs = decodedCookie.split(';');
-  console.log(crumbs);
-  for(var crumb in crumbs) {
-    console.log(crumb);
-    var result = crumb.match(name_pattern);
-    console.log(result);
-    if (result != null) {
-      return result[0];
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-  } return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
+
+// function getCookie(name) {
+//   var name_pattern = new RegExp(name + "=(.*)");
+//   var decodedCookie = decodeURIComponent(document.cookie); // why needed?
+//   //console.log("Decoded: " + decodedCookie);
+//   var crumbs = decodedCookie.split(';');
+//   console.log(crumbs);
+//   for(var crumb in crumbs) {
+//     console.log(crumb);
+//     var result = crumb.match(name_pattern);
+//     console.log(result);
+//     if (result != null) {
+//       return result[0];
+//     }
+//   } return "";
+// }
+
 function saveToCookies(changes) {
   for (var change in changes) {
     makeCookie(change, changes[change]);
