40aca9592e0406e7668f63e8be1073e76c690685
cmking54 <cmking5496@gmail.com>
2019-09-11 13:10:54 -0400

    Transpage cookie reload refactor; still broken

---
+++ js/cookie.js
@@ -0,0 +1,24 @@
+function makeCookie(key, value, hours_till_expire=1) {
+  var date = new Date();
+  date.setTime(date.getTime() + (hours_till_expire*60*60*1000));
+  var expires = "expires=" + date.toGMTString();
+  document.cookie = key + "=" + value + ";" + expires + ";path=/;";
+}
+
+function getCookie(name) {
+  var name_pattern = new RegExp(name + "=(*)");
+  var decodedCookie = decodeURIComponent(document.cookie); // why needed?
+  console.log("Decoded: " + decodedCookie);
+  var crumbs = decodedCookie.split(';');
+  for(var crumb in crumbs) {
+    var result = crumb.match(name_pattern);
+    if (result != null) {
+      return result[0];
+    }
+  } return "";
+}
+function saveToCookies(changes) {
+  for (var change in changes) {
+    makeCookie(change, changes[change]);
+  }
+}
--- js/homepage_form.js
+++ js/homepage_form.js
@@ -1,48 +1,14 @@
 'use strict';
 var changes = {};
 function save_school_page() {
-  console.log(document.cookie);
-  console.log(changes);
-  for (var change in changes) {
-    makeCookie(change, changes[change]);
-  }
-  console.log(document.cookie);
-
+  saveToCookies(changes);
 }
 function change_primary_color(choice) {
   var hex_color = "#" + choice;
   document.getElementsByTagName('body')[0].style.backgroundColor = hex_color;
-  //checkChanged('title');
   changes['primary_color'] = hex_color;
 }
 function changeTitle(choice) {
   document.getElementsByTagName('h1')[0].innerText = choice.value;
   changes['title'] = choice.value;
-}
-function checkChanged(prop) {
-  if (!changed.hasOwnProperty(prop)) changed[prop] = true;
-}
-function makeCookie(key, value, hours_till_expire=1) {
-  var date = new Date();
-  date.setTime(date.getTime() + (hours_till_expire*60*60*1000));
-  var expires = "expires=" + date.toGMTString();
-  console.log(key, value);
-  document.cookie = key + "=" + value + ";" + expires + ";path=/;";
-  // initial setup
-}
-
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
 }
